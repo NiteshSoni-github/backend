e325c80a464e6a4f56bf20c7d2e9a6049fbec417
akshay_solanki <akshaysolanki057@gmail.com>
2019-10-06 23:33:26 +0530

    done

--- blogfrontend/src/components/manageblog.vue
+++ blogfrontend/src/components/manageblog.vue
@@ -4,219 +4,251 @@
       <v-row>
         <v-col cols="12" md="2" class="d-none d-md-block"></v-col>
         <v-col cols="12" md="8" class="d-none d-md-block">
-          <v-row>
-            <v-col cols="12">
-              <v-data-table
-                :headers="headers"
-                :items="desserts"
-                sort-by="calories"
-                class="elevation-1"
-                v-model="selected"
+          <v-tabs slot="extension" v-model="tabs" background-color="transparent" centered>
+            <v-tab v-for="n in 3" :key="n">{{ items[n] }}</v-tab>
+          </v-tabs>
+
+          <v-tabs-items v-model="tabs">
+
+
+
+
+            <v-tab-item>
+              <v-row>
+                
+                <v-col cols="12" class="d-none d-md-block">
+                  <v-row>
+                    <v-col cols="12">
+                      <v-data-table
+                        :headers="headers"
+                        :items="desserts"
+                        sort-by="calories"
+                        class="elevation-1"
+                      >
+                        <!-- v-model="selected"
                 :single-select="singleSelect"
                 item-key="name"
-                show-select
-              >
-                <template v-slot:top>
-                  <v-toolbar flat color="white">
-                    <v-toolbar-title>My Favourite Blogs</v-toolbar-title>
-                    <v-divider class="mx-4" inset vertical></v-divider>
-                    <div class="flex-grow-1"></div>
-                    <v-text-field
-                      v-model="search"
-                      append-icon="search"
-                      label="Search"
-                      single-line
-                      hide-details
-                    ></v-text-field>
-                    <div class="flex-grow-1"></div>
-                    <router-link to="/createblog" exact>
-                      <v-btn color="primary" dark class="mb-2" v-on="on">
-                        <v-icon left>share</v-icon>Share Selected
-                      </v-btn>
-                    </router-link>
-                  </v-toolbar>
-                  <hr class="my-6" />
-                </template>
-                <template v-slot:item.action="{ item }">
-                  <router-link to="/readblog" exact>
-                    <v-icon small class="mr-2">remove_red_eye</v-icon>
-                  </router-link>
+                        show-select-->
 
-                  <v-icon small @click.stop="dialog = true">delete</v-icon>
-                </template>
-                <template v-slot:no-data>
-                  <v-btn color="primary" @click="initialize">Reset</v-btn>
-                </template>
-                <template v-slot:item.status="{ item }">
-                  <v-chip color="pink" small dark>{{ item.statusfavourite }}</v-chip>
-                </template>
-              </v-data-table>
-            </v-col>
-          </v-row>
+                        <template v-slot:top>
+                          <v-toolbar flat color="white">
+                            <v-toolbar-title>My Drafted Blogs</v-toolbar-title>
+                            <v-divider class="mx-4" inset vertical></v-divider>
+                            <div class="flex-grow-1"></div>
+                            <v-text-field
+                              v-model="search"
+                              append-icon="search"
+                              label="Search"
+                              single-line
+                              hide-details
+                            ></v-text-field>
+                            <div class="flex-grow-1"></div>
+                          </v-toolbar>
+                          <hr class="my-6" />
+                        </template>
+                        <template v-slot:item.action="{ item }">
+                          <router-link to="/editblog" exact>
+                            <v-icon small class="mr-2">edit</v-icon>
+                          </router-link>
+
+                          <v-icon small @click.stop="dialog = true">delete</v-icon>
+                        </template>
+                        <template v-slot:no-data>
+                          <v-btn color="primary" @click="initialize">Reset</v-btn>
+                        </template>
+                        <template v-slot:item.status="{ item }">
+                          <v-chip color="green" small dark>{{ item.statusdraft }}</v-chip>
+                        </template>
+                      </v-data-table>
+                    </v-col>
+                  </v-row>
+                </v-col>
+
+                <v-col cols="12" md="2" class="d-none d-md-block"></v-col>
+
+                <v-dialog v-model="dialog" max-width="390">
+                  <v-card>
+                    <v-card-title class="headline">Use Google's location service?</v-card-title>
+
+                    <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+
+                    <v-card-actions>
+                      <div class="flex-grow-1"></div>
+
+                      <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
+
+                      <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-row>
+
+              
+            </v-tab-item>
+
+
+
+
+
+            <v-tab-item>
+              <v-row>
+                <v-col cols="12">
+                  <v-data-table
+                    :headers="headers"
+                    :items="desserts"
+                    sort-by="calories"
+                    class="elevation-1"
+                    v-model="selected"
+                    :single-select="singleSelect"
+                    item-key="name"
+                    show-select
+                  >
+                    <template v-slot:top>
+                      <v-toolbar flat color="white">
+                        <v-toolbar-title>My Favourite Blogs</v-toolbar-title>
+                        <v-divider class="mx-4" inset vertical></v-divider>
+                        <div class="flex-grow-1"></div>
+                        <v-text-field
+                          v-model="search"
+                          append-icon="search"
+                          label="Search"
+                          single-line
+                          hide-details
+                        ></v-text-field>
+                        <div class="flex-grow-1"></div>
+                        <router-link to="/createblog" exact>
+                          <v-btn color="primary" dark class="mb-2" v-on="on">
+                            <v-icon left>share</v-icon>Share Selected
+                          </v-btn>
+                        </router-link>
+                      </v-toolbar>
+                      <hr class="my-6" />
+                    </template>
+                    <template v-slot:item.action="{ item }">
+                      <router-link to="/readblog" exact>
+                        <v-icon small class="mr-2">remove_red_eye</v-icon>
+                      </router-link>
+
+                      <v-icon small @click.stop="dialog = true">delete</v-icon>
+                    </template>
+                    <template v-slot:no-data>
+                      <v-btn color="primary" @click="initialize">Reset</v-btn>
+                    </template>
+                    <template v-slot:item.status="{ item }">
+                      <v-chip color="pink" small dark>{{ item.statusfavourite }}</v-chip>
+                    </template>
+                  </v-data-table>
+                </v-col>
+              </v-row>
+
+               <v-col cols="12" md="2" class="d-none d-md-block"></v-col>
+
+              <v-dialog v-model="dialog" max-width="390">
+                <v-card>
+                  <v-card-title class="headline">Use Google's location service?</v-card-title>
+
+                  <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+
+                  <v-card-actions>
+                    <div class="flex-grow-1"></div>
+
+                    <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
+
+                    <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+
+            </v-tab-item>
+
+
+
+
+            <v-tab-item>
+              <v-row>
+                <v-col cols="12">
+                  <v-data-table
+                    :headers="headers"
+                    :items="desserts"
+                    sort-by="calories"
+                    class="elevation-1"
+                    v-model="selected"
+                    :single-select="singleSelect"
+                    item-key="name"
+                    show-select
+                  >
+                    <template v-slot:top>
+                      <v-toolbar flat color="white">
+                        <v-toolbar-title>My Publishd Blogs</v-toolbar-title>
+                        <v-divider class="mx-4" inset vertical></v-divider>
+                        <div class="flex-grow-1"></div>
+                        <v-text-field
+                          v-model="search"
+                          append-icon="search"
+                          label="Search"
+                          single-line
+                          hide-details
+                        ></v-text-field>
+                        <div class="flex-grow-1"></div>
+                        <router-link to="/createblog" exact>
+                          <v-btn color="primary" dark class="mb-2" v-on="on">
+                            <v-icon left>share</v-icon>Share Selected
+                          </v-btn>
+                        </router-link>
+                      </v-toolbar>
+                      <hr class="my-6" />
+                    </template>
+                    <template v-slot:item.action="{ item }">
+                      <router-link to="/readblog" exact>
+                        <v-icon small class="mr-2">remove_red_eye</v-icon>
+                      </router-link>
+                      <router-link to="/editblog" exact>
+                        <v-icon small class="mr-2">edit</v-icon>
+                      </router-link>
+
+                      <v-icon small @click.stop="dialog = true">delete</v-icon>
+                    </template>
+                    <template v-slot:no-data>
+                      <v-btn color="primary" @click="initialize">Reset</v-btn>
+                    </template>
+                    <template v-slot:item.status="{ item }">
+                      <v-chip color="blue" small dark>{{ item.statuspublished }}</v-chip>
+                    </template>
+                  </v-data-table>
+                </v-col>
+              </v-row>
+
+              <v-col cols="12" md="2" class="d-none d-md-block"></v-col>
+
+              <v-dialog v-model="dialog" max-width="390">
+                <v-card>
+                  <v-card-title class="headline">Use Google's location service?</v-card-title>
+
+                  <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+
+                  <v-card-actions>
+                    <div class="flex-grow-1"></div>
+
+                    <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
+
+                    <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-tab-item>
+
+
+          </v-tabs-items>
+          <!--------->
+
+          <!---------->
         </v-col>
-
-        <v-col cols="12" md="2" class="d-none d-md-block"></v-col>
-
-        <v-dialog v-model="dialog" max-width="390">
-          <v-card>
-            <v-card-title class="headline">Use Google's location service?</v-card-title>
-
-            <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
-
-            <v-card-actions>
-              <div class="flex-grow-1"></div>
-
-              <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
-
-              <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-row>
-      <v-row>
-        <v-col cols="12" md="2" class="d-none d-md-block"></v-col>
-        <v-col cols="12" md="8" class="d-none d-md-block">
-          <v-row>
-            <v-col cols="12">
-              <v-data-table
-                :headers="headers"
-                :items="desserts"
-                sort-by="calories"
-                class="elevation-1"
-            
-              >     <!-- v-model="selected"
-                :single-select="singleSelect"
-                item-key="name"
-                show-select-->
-                <template v-slot:top>
-                  <v-toolbar flat color="white">
-                    <v-toolbar-title>My Drafted Blogs</v-toolbar-title>
-                    <v-divider class="mx-4" inset vertical></v-divider>
-                    <div class="flex-grow-1"></div>
-                    <v-text-field
-                      v-model="search"
-                      append-icon="search"
-                      label="Search"
-                      single-line
-                      hide-details
-                    ></v-text-field>
-                    <div class="flex-grow-1"></div>
-                 
-                  </v-toolbar>
-                  <hr class="my-6" />
-                </template>
-                <template v-slot:item.action="{ item }">
-                  <router-link to="/editblog" exact>
-                    <v-icon small class="mr-2">edit</v-icon>
-                  </router-link>
-
-                  <v-icon small @click.stop="dialog = true">delete</v-icon>
-                </template>
-                <template v-slot:no-data>
-                  <v-btn color="primary" @click="initialize">Reset</v-btn>
-                </template>
-                <template v-slot:item.status="{ item }">
-                  <v-chip color="green" small dark>{{ item.statusdraft }}</v-chip>
-                </template>
-              </v-data-table>
-            </v-col>
-          </v-row>
-        </v-col>
-
-        <v-col cols="12" md="2" class="d-none d-md-block"></v-col>
-
-        <v-dialog v-model="dialog" max-width="390">
-          <v-card>
-            <v-card-title class="headline">Use Google's location service?</v-card-title>
-
-            <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
-
-            <v-card-actions>
-              <div class="flex-grow-1"></div>
-
-              <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
-
-              <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
       </v-row>
 
-      <v-row>
-        <v-col cols="12" md="2" class="d-none d-md-block"></v-col>
-        <v-col cols="12" md="8" class="d-none d-md-block">
-          <v-row>
-            <v-col cols="12">
-              <v-data-table
-                :headers="headers"
-                :items="desserts"
-                sort-by="calories"
-                class="elevation-1"
-                v-model="selected"
-                :single-select="singleSelect"
-                item-key="name"
-                show-select
-              >
-                <template v-slot:top>
-                  <v-toolbar flat color="white">
-                    <v-toolbar-title>My Publishd Blogs</v-toolbar-title>
-                    <v-divider class="mx-4" inset vertical></v-divider>
-                    <div class="flex-grow-1"></div>
-                    <v-text-field
-                      v-model="search"
-                      append-icon="search"
-                      label="Search"
-                      single-line
-                      hide-details
-                    ></v-text-field>
-                    <div class="flex-grow-1"></div>
-                    <router-link to="/createblog" exact>
-                      <v-btn color="primary" dark class="mb-2" v-on="on">
-                        <v-icon left>share</v-icon>Share Selected
-                      </v-btn>
-                    </router-link>
-                  </v-toolbar>
-                  <hr class="my-6" />
-                </template>
-                <template v-slot:item.action="{ item }">
-                  <router-link to="/readblog" exact>
-                    <v-icon small class="mr-2">remove_red_eye</v-icon>
-                  </router-link>
-                  <router-link to="/editblog" exact>
-                    <v-icon small class="mr-2">edit</v-icon>
-                  </router-link>
+      <!---------->
 
-                  <v-icon small @click.stop="dialog = true">delete</v-icon>
-                </template>
-                <template v-slot:no-data>
-                  <v-btn color="primary" @click="initialize">Reset</v-btn>
-                </template>
-                <template v-slot:item.status="{ item }">
-                  <v-chip color="blue" small dark>{{ item.statuspublished }}</v-chip>
-                </template>
-              </v-data-table>
-            </v-col>
-          </v-row>
-        </v-col>
-
-        <v-col cols="12" md="2" class="d-none d-md-block"></v-col>
-
-        <v-dialog v-model="dialog" max-width="390">
-          <v-card>
-            <v-card-title class="headline">Use Google's location service?</v-card-title>
-
-            <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
-
-            <v-card-actions>
-              <div class="flex-grow-1"></div>
-
-              <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
-
-              <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-row>
+      <!---------->
+     
     </v-container>
   </div>
 </template>
@@ -224,6 +256,9 @@
 <script>
 export default {
   data: () => ({
+    tabs: null,
+    items: ["ss","Draft", "My Favourit",  "Published"],
+
     singleSelect: false,
     selected: [],
     dialog: false,
