dd05db34e57422b305925c1849ca239bc032cc6a
akshay_solanki <akshaysolanki057@gmail.com>
2019-10-08 15:04:48 +0530

    okay login and registration page

--- blogfrontend/src/views/login.vue
+++ blogfrontend/src/views/login.vue
@@ -42,7 +42,77 @@
 
             <v-card-text class="text-center">
               Have not account yet?
+              
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn small text color="primary" dark v-on="on">Sign up</v-btn>
+
+      </template>
+      <v-card>
+          <v-card-title>
+              <v-img
+                src="./../../public/logo.png"
+                alt="avatar"
+                class="mx-auto"
+                max-width="140"
+                max-height="50"
+              ></v-img>
+            </v-card-title>
+            <v-divider></v-divider>
+
+            <v-card-text class="headline text-center">Sign Up</v-card-text>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="Legal first name*" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  label="Legal last name*"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Email*" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Password*" type="password" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-select
+                  :items="['0-17', '18-29', '30-54', '54+']"
+                  label="Age*"
+                  required
+                ></v-select>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-autocomplete
+                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                  label="Interests"
+                  multiple
+                ></v-autocomplete>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <div class="flex-grow-1"></div>
+          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  
+              <!--
               <v-btn text small color="primary">Signup</v-btn>
+              -->
             </v-card-text>
           </v-card>
         </v-col>
@@ -55,7 +125,7 @@
 export default {
   data() {
     return {
-   
+      dialog: false,
       email: "",
       emailRules: [
         // v => !!v || "E-mail is required",
--- blogfrontend/src/views/register.vue
+++ blogfrontend/src/views/register.vue
@@ -1,132 +1,68 @@
 <template>
-<div>
-    <v-container fluid>
-      <v-row>
-        <v-col cols="12" md="3" class="d-none d-md-block"></v-col>
-        <v-col md="6" cols="12">
-        <v-card
-    class="mx-auto"
-  >  
-  <v-stepper v-model="e1" class="mt-12" editable>
-    <v-stepper-header>
-      <v-stepper-step :complete="e1 > 1"  step="1">General</v-stepper-step>
-
-      <v-divider></v-divider>
-
-      <v-stepper-step :complete="e1 > 2"  step="2">Account</v-stepper-step>
-
-      <v-divider></v-divider>
-
-      <v-stepper-step  step="3">Finish</v-stepper-step>
-    </v-stepper-header>
-     </v-stepper>
-<v-window v-model="step">
-    
-  
-      <v-window-item :value="1">
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
         <v-card-text>
-          <v-text-field
-            label="Email"
-            value="john@vuetifyjs.com"
-          ></v-text-field>
-         
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="Legal first name*" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  label="Legal last name*"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Email*" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Password*" type="password" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-select
+                  :items="['0-17', '18-29', '30-54', '54+']"
+                  label="Age*"
+                  required
+                ></v-select>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-autocomplete
+                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                  label="Interests"
+                  multiple
+                ></v-autocomplete>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
         </v-card-text>
-      </v-window-item>
-
-      
-
-   
-      <v-window-item :value="2">
-        <v-card-text>
-          <v-text-field
-            label="Password"
-            type="password"
-          ></v-text-field>
-          <v-text-field
-            label="Confirm Password"
-            type="password"
-          ></v-text-field>
-          <span class="caption grey--text text--darken-1">
-            Please enter a password for your account
-          </span>
-        </v-card-text>
-      </v-window-item>
-    
-       
-      <v-window-item :value="3">
-        <div class="pa-4 text-center">
-          <v-img
-            class="mb-4"
-            contain
-            height="128"
-            src="https://cdn.vuetifyjs.com/images/logos/v.svg"
-          ></v-img>
-          <h3 class="title font-weight-light mb-2">Welcome to Vuetify</h3>
-          <span class="caption grey--text">Thanks for signing up!</span>
-        </div>
-      </v-window-item>
-
-</v-window>
- <v-divider></v-divider>
-     <v-card-actions>
-      
-      <v-btn
-        :disabled="step === 1"
-        text
-        @click="Decrese"
-        
-      >
-        Back
-      </v-btn>
-      <div class="flex-grow-1"></div>
-      <v-btn
-        :disabled="step === 3"
-        color="primary"
-        depressed
-        @click="increse"
-        
-      >
-        Next
-      </v-btn>
-    </v-card-actions>
- 
- 
- </v-card>
-        </v-col>
-        <v-col cols="12" md="3" class="d-none d-md-block"></v-col>
-      </v-row>
-    </v-container>
-    
-  </div>
+        <v-card-actions>
+          <div class="flex-grow-1"></div>
+          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
 </template>
+
 <script>
   export default {
-    data () {
-      return {
-        e1: 1,
-           step: 1,
-      }
-    },
-     computed: {
-      currentTitle () {
-        switch (this.step) {
-          case 1: return 'Sign-up'
-          case 2: return 'Create a password'
-          default: return 'Account created'
-        }
-      },
-    },
-    methods: {
-    increse: function()  {
-    this.step++;
-    this.e1++;
-   
-  },
-  Decrese: function()  {
-    this.step--;
-    this.e1--;
-    
-  }
-}
+    data: () => ({
+      dialog: false,
+    }),
   }
 </script>
