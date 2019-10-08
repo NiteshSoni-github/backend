844c75c801e0f0b26100c100cde51b6ff8e23ce1
akshay_solanki <akshaysolanki057@gmail.com>
2019-10-08 15:37:11 +0530

    login and sign up page complete

--- blogfrontend/src/views/login.vue
+++ blogfrontend/src/views/login.vue
@@ -1,123 +1,147 @@
-<template >
+<template>
   <div class="backimage">
-    <v-container fluid >
-      <v-row>
-        <v-col cols="12" md="3" class="d-none d-md-block"></v-col>
-        <v-col md="6" cols="12" >
-          <v-card class="mx-auto mt-12" elevation="1" max-width="600" >
-            <v-card-title>
-              <v-img
-                src="./../../public/logo.png"
-                alt="avatar"
-                class="mx-auto"
-                max-width="140"
-                max-height="50"
-              ></v-img>
-            </v-card-title>
-            <v-divider></v-divider>
+    <v-container fluid>
+      <div>
+        <v-row>
+          <v-col cols="12" md="3" class="d-none d-md-block"></v-col>
+          <v-col md="6" cols="12">
+            <v-card class="mx-auto mt-12" elevation="1" max-width="600">
+              <v-card-title>
+                <v-img
+                  src="./../../public/logo.png"
+                  alt="avatar"
+                  class="mx-auto"
+                  max-width="140"
+                  max-height="50"
+                ></v-img>
+              </v-card-title>
+              <v-divider></v-divider>
 
-            <v-card-text class="headline text-center">Login</v-card-text>
-            <v-card-text>
-              <v-form class="mx-12">
-                <v-text-field :rules="emailRules" label="E-mail"></v-text-field>
-                <v-text-field
-                  :append-icon="show2 ? 'visibility' : 'visibility_off'"
-                  :rules="[rules.required, rules.min]"
-                  :type="show2 ? 'text' : 'password'"
-                  name="input-10-2"
-                  label="Password"
-                  @click:append="show2 = !show2"
-                ></v-text-field>
+              <v-card-text class="headline text-center">Login</v-card-text>
+              <v-card-text>
+                <v-form class="mx-12">
+                  <v-text-field :rules="emailRules" label="E-mail"></v-text-field>
+                  <v-text-field
+                    :append-icon="show2 ? 'visibility' : 'visibility_off'"
+                    :rules="[rules.required, rules.min]"
+                    :type="show2 ? 'text' : 'password'"
+                    name="input-10-2"
+                    label="Password"
+                    @click:append="show2 = !show2"
+                  ></v-text-field>
 
-                <p class="text-right">
-                  <router-link to="/editblog" exact>
-                  Forgot password?
-                  </router-link> 
+                  <p class="text-right">
+                    <v-dialog v-model="dialog1" persistent max-width="600px">
+                      <template v-slot:activator="{ on }">
+                        <v-btn small text color="primary" dark v-on="on">Forgot PAssword</v-btn>
+                      </template>
+                      <v-card>
+                        <v-card-title></v-card-title>
+
+                        <v-card-text class="headline text-center">Enter Your Email</v-card-text>
+                        <v-card-text>
+                          <v-container>
+                            <v-row>
+                              <v-col cols="12">
+                                <v-text-field label="Email*" required></v-text-field>
+                              </v-col>
+                            </v-row>
+                          </v-container>
+                        </v-card-text>
+                        <v-card-actions>
+                          <div class="flex-grow-1"></div>
+                          <v-btn color="primary" @click="dialog1 = false">Send</v-btn>
+                          <v-btn color="primary" @click="dialog1 = false">Close</v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
                   </p>
-                
-                <v-btn color="primary">Login</v-btn>
-              </v-form>
-            </v-card-text>
-            <v-divider class="mt-4"></v-divider>
 
-            <v-card-text class="text-center">
-              Have not account yet?
-              
-    <v-dialog v-model="dialog" persistent max-width="600px">
-      <template v-slot:activator="{ on }">
-        <v-btn small text color="primary" dark v-on="on">Sign up</v-btn>
+                  <v-btn color="primary">Login</v-btn>
+                </v-form>
+              </v-card-text>
+              <v-divider class="mt-4"></v-divider>
 
-      </template>
-      <v-card>
-          <v-card-title>
-              <v-img
-                src="./../../public/logo.png"
-                alt="avatar"
-                class="mx-auto"
-                max-width="140"
-                max-height="50"
-              ></v-img>
-            </v-card-title>
-            <v-divider></v-divider>
+              <v-card-text class="text-center">
+                Have not account yet?
+                <v-dialog v-model="dialog" persistent max-width="600px">
+                  <template v-slot:activator="{ on }">
+                    <v-btn small text color="primary" dark v-on="on">Sign up</v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-title>
+                      <v-img
+                        src="./../../public/logo.png"
+                        alt="avatar"
+                        class="mx-auto"
+                        max-width="140"
+                        max-height="50"
+                      ></v-img>
+                    </v-card-title>
+                    <v-divider></v-divider>
 
-            <v-card-text class="headline text-center">Sign Up</v-card-text>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Legal first name*" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field
-                  label="Legal last name*"
-                  hint="example of persistent helper text"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Email*" required></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field label="Password*" type="password" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  label="Age*"
-                  required
-                ></v-select>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-autocomplete
-                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                  label="Interests"
-                  multiple
-                ></v-autocomplete>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <div class="flex-grow-1"></div>
-          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  
-              <!--
+                    <v-card-text class="headline text-center">Sign Up</v-card-text>
+                    <v-card-text>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12" sm="6" md="4">
+                            <v-text-field label="Legal first name*" required></v-text-field>
+                          </v-col>
+                          <v-col cols="12" sm="6" md="4">
+                            <v-text-field
+                              label="Legal middle name"
+                              hint="example of helper text only on focus"
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="12" sm="6" md="4">
+                            <v-text-field
+                              label="Legal last name*"
+                              hint="example of persistent helper text"
+                              persistent-hint
+                              required
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="12">
+                            <v-text-field label="Email*" required></v-text-field>
+                          </v-col>
+                          <v-col cols="12">
+                            <v-text-field label="Password*" type="password" required></v-text-field>
+                          </v-col>
+                          <v-col cols="12" sm="6">
+                            <v-select
+                              :items="['0-17', '18-29', '30-54', '54+']"
+                              label="Age*"
+                              required
+                            ></v-select>
+                          </v-col>
+                          <v-col cols="12" sm="6">
+                            <v-autocomplete
+                              :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                              label="Interests"
+                              multiple
+                            ></v-autocomplete>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                      <small>*indicates required field</small>
+                    </v-card-text>
+                    <v-card-actions>
+                      <div class="flex-grow-1"></div>
+                      <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+                      <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+
+                <!--
               <v-btn text small color="primary">Signup</v-btn>
-              -->
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col cols="12" md="3" class="d-none d-md-block"></v-col>
-      </v-row>
+                -->
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="3" class="d-none d-md-block"></v-col>
+        </v-row>
+      </div>
     </v-container>
   </div>
 </template>
@@ -126,6 +150,7 @@ export default {
   data() {
     return {
       dialog: false,
+      dialog1: false,
       email: "",
       emailRules: [
         // v => !!v || "E-mail is required",
@@ -146,10 +171,11 @@ export default {
 };
 </script>
 <style >
-.backimage{
+.backimage {
   height: 95%;
-  top:0;
-  background: url( 'https://wallpapercave.com/wp/tmx6W6N.png') no-repeat center center;
-background-size: cover;
+  top: 0;
+  background: url("https://wallpapercave.com/wp/tmx6W6N.png") no-repeat center
+    center;
+  background-size: cover;
 }
 </style>
